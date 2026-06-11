07f3f751befa898bfa2e59778ead65514a0f2e9f
wveroo <veroo@elixcore.com>
2026-06-12 05:48:47 +0900

    refactor(ui): Overlay 자동 리팩토링 및 규칙 적용

--- src/components/overlays/Overlay.tsx
+++ src/components/overlays/Overlay.tsx
@@ -2,9 +2,22 @@ import React, { forwardRef } from "react";
 import styled from "@emotion/styled";
 import { applyAnimation, fixedCover, overlayShow } from "../../styles";
 
-export interface OverlayProps extends React.HTMLAttributes<HTMLDivElement> {
+/**
+ * Overlay primitive에 전달할 레이어 순서와 표준 div 속성을 정의합니다.
+ *
+ * 스타일은 theme/mixin으로만 관리하기 위해 React의 `style` prop은 제외합니다.
+ */
+export interface OverlayProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "style"> {
   zIndex?: number;
 }
+
+/**
+ * 모달, 알럿, 드로어 뒤에 표시되는 전역 dim overlay입니다.
+ *
+ * fixed cover mixin과 overlay animation을 적용하며, ref와 접근성 속성을 그대로
+ * 전달할 수 있습니다.
+ */
 export const Overlay = forwardRef<HTMLDivElement, OverlayProps>(
   ({ zIndex, ...props }, ref) => {
     return <StyledOverlay ref={ref} $zIndex={zIndex} {...props} />;
