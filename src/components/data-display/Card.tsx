c70def92011a80e9f3ab2c52a46a21aa9aa23597
wveroo <veroo@elixcore.com>
2026-06-12 05:36:50 +0900

    refactor(ui): Card 자동 리팩토링 및 규칙 적용

--- src/components/data-display/Card.tsx
+++ src/components/data-display/Card.tsx
@@ -7,8 +7,19 @@ import {
   SectionFooter,
 } from "../layout";
 
+/**
+ * Card root가 상속하는 section HTML 속성입니다.
+ *
+ * Card 자체는 spacing을 만들지 않고, 내부 Header/Body/Footer preset의 gap과 padding을
+ * 통해 구조화됩니다.
+ */
 export interface CardProps extends React.HTMLAttributes<HTMLElement> {}
 
+/**
+ * Card compound component의 root 영역입니다.
+ *
+ * layout `Section`을 기반으로 surface, border, shadow를 일관되게 적용합니다.
+ */
 const CardRoot = forwardRef<HTMLElement, CardProps>(
   ({ children, ...props }, ref) => (
     <Section ref={ref} {...props}>
@@ -18,7 +29,18 @@ const CardRoot = forwardRef<HTMLElement, CardProps>(
 );
 CardRoot.displayName = "Card";
 
+/**
+ * Card body가 상속하는 div HTML 속성입니다.
+ *
+ * 스크롤과 padding 정책은 layout `SectionBody`의 컨벤션을 따릅니다.
+ */
 export interface CardBodyProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+/**
+ * Card의 주요 콘텐츠 영역입니다.
+ *
+ * Header/Footer와 조합해 예측 가능한 패널 구조를 구성합니다.
+ */
 const CardBody = forwardRef<HTMLDivElement, CardBodyProps>(
   ({ children, ...props }, ref) => (
     <SectionBody ref={ref} {...props}>
@@ -28,6 +50,9 @@ const CardBody = forwardRef<HTMLDivElement, CardBodyProps>(
 );
 CardBody.displayName = "CardBody";
 
+/**
+ * Header, TitleGroup, Body, Footer를 포함하는 compound Card API입니다.
+ */
 export const Card = Object.assign(CardRoot, {
   Header: SectionHeader,
   TitleGroup: SectionTitleGroup,
